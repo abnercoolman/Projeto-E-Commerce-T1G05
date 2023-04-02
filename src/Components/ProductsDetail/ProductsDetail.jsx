1c3b369e1754b3c4eb2db3492d8a6a102c265576
abnercoolman <105078332+abnercoolman@users.noreply.github.com>
2023-04-02 17:04:05 -0300

    feat(Products): Finalizado HTML/SASS/JS
    OBSERVAÇÕES GERAIS: View “Products” finalizada, juntamente com seu component “ProductsDetail”.
    
    1. Pasta “Components”
      1.1 Pasta “ProductsDetail”
      •	Adição do component “ProductsDetail”;
      •	Funcionalidade Campo de cor e Texto de cor – foi criado um ternário para que quando as propriedades “colorsText” e “colors”, viessem vazias para um determinado produto do “products.mock.js”, o texto “Cor:” desaparecesse, dessa forma, foi feita a seguinte lógica: {data.colorsText.length === 0 ? " " : "Cor:"}.
    
      1.2. Pasta “views”
            1.2.1 Pasta “Products”
            •	No arquivo Products.jsx, substituição da div “product__teste” entre os components <HeaderMenu> e <Footer>, pela chamada do component <ProductsDetail />;
            •	Chamada de dados através do useParams do Hooks, dentro do functional component {const { productId } = useParams();} e {const selectedProduct = data.find(product => product.id == productId);};
            •	Requisição de dados dentro da chamada do component: {<ProductsDetail data={selectedProduct} />};
            •	No arquivo Products.jsx, modificação da linha de código {document.title = 'Product - DNC Store';} para {document.title = selectedProduct.title;};
            •	Exclusão do conteúdo do arquivo “index.scss” referente ao arquivo Products.jsx.

---
+++ src/Components/ProductsDetail/ProductsDetail.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import cartIcon from "../../assets/shopping_cart.png";
+import "./index.scss";
+
+const ProductDetail = ({ data }) => {
+    //console.log(data);
+    return (
+        <div className="product-detail">
+            <div className="product-detail__left-side">
+                <div className="product-detail__left-side__card">
+                    <img src={data.imgPathDetail} alt={data.title} />
+                    <p>{data.title}</p>
+                </div>
+                <div className="product-detail__left-side__description">
+                    <h1>Descrição</h1>
+                    <p>{data.descripition}</p>
+                </div>
+            </div>
+            <div className="product-detail__right-side">
+                <h1>{data.title}</h1>
+                <h2>{data.price}</h2>
+                <p>
+                    {data.colorsText.length === 0
+                        ? ""
+                        : "Cor:"
+                    }
+                    {" "}
+                    {data.colorsText.length === 1
+                        ? data.colorsText.map((color, index) => (
+                            <span key={index}>{color}</span>
+                        ))
+                        : data.colorsText.map((color, index) =>
+                            index + 1 === data.colorsText.length ? (
+                                <span key={index}>{color}</span>
+                            ) : (
+                                <span key={index}>{color}, </span>
+                            )
+                        )}
+                </p>
+                <div className="product-detail__right-side__cube-colors">
+                    {data.colors.map((color) => (
+                        <div
+                            style={{
+                                width: "40px",
+                                height: "40px",
+                                borderRadius: "10px",
+                                backgroundColor: color,
+                            }}
+                        ></div>
+                    ))}
+                </div>
+
+                <button>
+                    <img src={cartIcon} alt="Cart Icon" />
+                    <Link to={`/pay/${data.id}`}>Adicionar ao carrinho</Link>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductDetail;
